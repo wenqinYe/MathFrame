e9ac8280f7fa81b04dd25d17fe0a4333f0ffd943
wenqinYe <samcarbet@sams-MacBook-Pro.local>
2013-10-12 21:55:46 -0400

    added math framework

---
+++ mathFrame/mathFrame/MathFrameWork.h
@@ -0,0 +1,13 @@
+//
+//  MathFrameWork.h
+//  mathFrame
+//
+//  Created by Wenqin Ye on 2013-10-12.
+//  Copyright (c) 2013 testing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MathFrameWork : NSObject
+
+@end
---
+++ mathFrame/mathFrame/MathFrameWork.m
@@ -0,0 +1,13 @@
+//
+//  MathFrameWork.m
+//  mathFrame
+//
+//  Created by Wenqin Ye on 2013-10-12.
+//  Copyright (c) 2013 testing. All rights reserved.
+//
+
+#import "MathFrameWork.h"
+
+@implementation MathFrameWork
+
+@end

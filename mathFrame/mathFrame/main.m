41e7486e3309035fb62891431acb57bd31963344
wenqinYe <samcarbet@sams-MacBook-Pro.local>
2013-10-12 21:51:54 -0400

    Initial Commit

---
+++ mathFrame/mathFrame/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mathFrame
+//
+//  Created by Wenqin Ye on 2013-10-12.
+//  Copyright (c) 2013 testing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
